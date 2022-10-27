975c967e378366ef53f8aebbc5c0961a85f72199
SarahBeFamily <sarah@befamily.it>
2022-10-27 15:30:14 +0200

    commit temp

--- templates/sections/meta.php
+++ templates/sections/meta.php
@@ -3,10 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />    <title>GESAC template by BeFamily</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />    <title>GESAC templates</title>
     <meta name="description" content="GESAC frontend templates">
     <meta name="keywords" content="GESAC templates">
-    <meta name="author" content="BeFamily">
     <link rel="stylesheet" type="text/css" href="dist/app.css">
 </head>
 
