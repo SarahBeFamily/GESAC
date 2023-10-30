a87693a52d1e2b67a1afd68e5f127acfead03561
Diego Purpo <diego@infocube.it>
2023-10-30 17:38:46 +0100

    wimob

---
+++ templates/wimob.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Bandi template
+ */
+?>
+
+    <section>
+        <iframe width="100%" height="100%" id="iframe" style="width: 100vw; min-height:99vh; display: block; overflow:scroll;" name="iframe"
+                src="https://www.wimob.it/link/pag5/?layout=horizontal&hide_bar=true"></iframe>
+    </section>
+
+
+<?php
+// Include the cta section "Download App"
+include_section('cta-download-app');
+
+// Include the cta section "Newsletter"
+include_section('cta-newsletter');
+?>
